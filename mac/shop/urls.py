e677ad8f0a5d848a1772835947ef00bac9817d09
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-08-27 18:00:47 +0530

    added a file

---
+++ mac/shop/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name='ShopHome'),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackerStatus"),
+    path("search/", views.search, name="Search"),
+    path("products/<int:myid>", views.products, name="Products"),
+    path("checkout/", views.checkout, name="Checkout"),
+    path('register/',views.register, name="register"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+]
