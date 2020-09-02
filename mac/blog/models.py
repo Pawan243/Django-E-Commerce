fed8e8228bd21bfedbdb2128667e6428638e03ef
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-02 21:33:34 +0530

    modified model.py

--- mac/blog/models.py
+++ mac/blog/models.py
@@ -11,7 +11,18 @@ class Blogpost(models.Model):
     head2 = models.CharField(max_length=500, default="")
     chead2 = models.CharField(max_length=5000, default="")
     pub_date = models.DateField()
-    thumbnail = models.ImageField(upload_to='shop/images', default="")
+    thumbnail = models.ImageField(upload_to='blog/images', default="")
 
     def __str__(self):
         return self.title
+
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=70, default="")
+    phone = models.CharField(max_length=70, default="")
+    desc = models.CharField(max_length=500, default="")
+
+
+    def __str__(self):
+        return self.name
