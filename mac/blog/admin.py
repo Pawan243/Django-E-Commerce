347f6e68c65528fbb3fe1554d8c6c14fa97f2b0a
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-02 21:31:41 +0530

    modified addmin.py

--- mac/blog/admin.py
+++ mac/blog/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Blogpost
+from .models import Blogpost ,Contact
 
 admin.site.register(Blogpost)
+admin.site.register(Contact)
