09141c68d3cb3524eb684721dfdf2a7152c8263a
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-03 08:31:01 +0530

    modified views.py

--- mac/blog/views.py
+++ mac/blog/views.py
@@ -8,9 +8,9 @@ def index(request):
     return render(request, 'blog/index.html')
 
 def blogpost(request, id):
-    post = Blogpost.objects.filter(post_id = id)
+    post = Blogpost.objects.filter(post_id = id)[0]
     print(post)
-    return render(request, 'blog/blogpost.html')
+    return render(request, 'blog/blogpost.html', {'post':post})
 
 
 def about(request):
