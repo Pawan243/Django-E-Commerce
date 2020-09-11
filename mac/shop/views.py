cb8a8525c10d825a735bf0a9a9e0513c411727bc
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-11 22:45:52 +0530

    modified dhop/views.py

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Product ,Contact, Orders, OrderUpdate
+from .models import Product, Contact, Orders, OrderUpdate
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 import json
- 
-from django.contrib.auth.models import User, auth 
+
+from django.contrib.auth.models import User, auth
 
 from math import ceil
 
@@ -29,7 +29,7 @@ def index(request):
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
         allProds.append([prod, range(1, nSlides), nSlides])
-    params = {'allProds':allProds}
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
 
@@ -38,7 +38,7 @@ def about(request):
 
 
 def contact(request):
-    if request.method=="POST":
+    if request.method == "POST":
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
         phone = request.POST.get('phone', '')
@@ -49,17 +49,19 @@ def contact(request):
 
 
 def tracker(request):
-    if request.method=="POST":
+    if request.method == "POST":
         orderId = request.POST.get('orderId', '')
         email = request.POST.get('email', '')
         try:
             order = Orders.objects.filter(order_id=orderId, email=email)
-            if len(order)>0:
+            if len(order) > 0:
                 update = OrderUpdate.objects.filter(order_id=orderId)
                 updates = []
                 for item in update:
-                    updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps([updates, order[0].items_json], default=str)
+                    updates.append(
+                        {'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(
+                        [updates, order[0].items_json], default=str)
                 return HttpResponse(response)
             else:
                 return HttpResponse('{}')
@@ -77,15 +79,16 @@ def products(request, myid):
 
     # Fetch the product using the id
     product = Product.objects.filter(id=myid)
-    return render(request, 'shop/prodView.html', {'product':product[0]})
+    return render(request, 'shop/prodView.html', {'product': product[0]})
 
 
 def checkout(request):
-    if request.method=="POST":
+    if request.method == "POST":
         items_json = request.POST.get('itemsJson', '')
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
-        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        address = request.POST.get('address1', '') + \
+            " " + request.POST.get('address2', '')
         city = request.POST.get('city', '')
         state = request.POST.get('state', '')
         zip_code = request.POST.get('zip_code', '')
@@ -93,15 +96,17 @@ def checkout(request):
         order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
                        state=state, zip_code=zip_code, phone=phone)
         order.save()
-        update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
+        update = OrderUpdate(order_id=order.order_id,
+                             update_desc="The order has been placed")
         update.save()
         thank = True
         id = order.order_id
-        return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
+        return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
     return render(request, 'shop/checkout.html')
 
+
 def login(request):
-    if request.method== 'POST':
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
@@ -114,14 +119,15 @@ def login(request):
             messages.info(request, 'Invalid credentials')
             return redirect('/shop/login')
 
+    else:
+        return render(request, 'shop/login.html')
 
-    else: 
-        return render(request, 'login.html')
 
 def logout(request):
 
     auth.logout(request)
     return redirect('/shop')
+
 
 def register(request):
     if request.method == "post":
@@ -131,29 +137,28 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         email = request.POST['email']
-        
+
         if password1 == password2:
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username Taken')
-                return redirect('shop/register')
+                return redirect('/shop/register')
 
             elif User.objects.filter(email=email).exists():
                 messages.info(request, 'email Taken')
-                return redirect('shop/register')
+                return redirect('/shop/register')
 
-            else:    
-                user = User.objects.create_user(first_name=first_name, last_name=last_name,username= username,  email=email,password=password1)
-                user.save();
+            else:
+                user = User.objects.create(
+                    first_name=first_name, last_name=last_name, username=username,  email=email, password=password1)
+                user.save()
                 print('user created')
-                return redirect('login')
-        
-        else: 
-            messages.info(request,'password not maching')
-            return redirect('register')
+                return redirect('/shop/login')
 
-        return  HttpResponseRedirect('/shop')
+        else:
+            messages.info(request, 'password not maching')
+            return redirect('/shop/register')
+
+        return HttpResponseRedirect('/shop')
 
     else:
-        return render(request,'register.html')
-
-
+        return render(request, 'shop/register.html')
