cccd5175d021ba2443286c5eb7a3e7e0b647c8db
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-09-29 13:49:15 +0530

    added file

---
+++ mac/media/shop/views.py
@@ -0,0 +1,160 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Product ,Contact, Orders, OrderUpdate
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+import json
+ 
+from django.contrib.auth.models import User, auth 
+
+from math import ceil
+
+# Create your views here.
+
+from django.shortcuts import render
+from .models import Product, Contact, Orders, OrderUpdate
+from math import ceil
+import json
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
+
+
+def about(request):
+    return render(request, 'shop/about.html' ,{'nbar':'AboutUs'})
+
+def contact(request):
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, 'shop/contact.html',{'nbar':'contact'})
+
+
+def tracker(request):
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps([updates, order[0].items_json], default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
+    return render(request, 'shop/tracker.html',{'nbar':'tracker'})
+
+
+def search(request):
+    return render(request, 'shop/search.html',{'nbar':'search'})
+
+
+def products(request, myid):
+
+    # Fetch the product using the id
+    product = Product.objects.filter(id=myid)
+    return render(request, 'shop/prodView.html', {'product':product[0]},{'nbar':'products/<int:myid>'})
+
+
+def checkout(request):
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
+        update.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
+    return render(request, 'shop/checkout.html',{'nbar':'checkout'})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/shop")
+
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('/shop/login')
+
+    else:
+        return render(request, 'shop/login.html',{'nbar':'login'})
+
+
+def logout(request):
+
+    auth.logout(request)
+    return redirect('/')
+
+
+def register(request):
+    if request.method == "post":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('/shop/register')
+
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'email Taken')
+                return redirect('/shop/register')
+
+            else:
+                user = User.objects.create(
+                    first_name=first_name, last_name=last_name, username=username,  email=email, password=password1)
+                user.save()
+                print('user created')
+                return redirect('/shop/login')
+
+        else:
+            messages.info(request, 'password not maching')
+            return redirect('/shop/register')
+
+        return HttpResponseRedirect('/shop')
+
+    else:
+        return render(request, 'shop/register.html',{'nbar':'register'})
+
+
