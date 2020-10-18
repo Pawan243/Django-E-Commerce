0ab956acce6c3e636f5ef299a16a69b71d4e363f
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2020-10-18 21:53:37 +0530

    modified

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    amount = models.IntegerField(default=0)
     category = models.CharField(max_length=50, default="")
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
@@ -30,7 +29,6 @@ class Contact(models.Model):
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
     items_json = models.CharField(max_length=5000)
-    amount=models.IntegerField(default=0)
     name = models.CharField(max_length=90)
     email = models.CharField(max_length=111)
     address = models.CharField(max_length=111)
